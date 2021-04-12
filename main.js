d5dcb759026e4a20cbed59005919ace1c2ed6012
Oliver van den Bosch <vanders06@icloud.com>
2021-04-12 16:40:43 +0100

    Update result field

--- main.js
+++ main.js
@@ -18,6 +18,17 @@ const convert = () => {
 
   let currency1 = selected[0].textContent;
   let currency2 = selected[1].textContent;
+  let amount = document.getElementById("amount");
+  let converted = document.getElementById('converted')
+  amount = amount.value;
 
-  
+  fetch(
+    `https://v6.exchangerate-api.com/v6/352b76412c86b5af5c9ccc3b/pair/${currency1}/${currency2}/${amount}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      converted.value = data.conversion_result;
+    });
 };
